34f54fe2768c95cb0b711c3c773d784ef082371d
Tommy Huang <tommyhuang999@gmail.com>
2026-01-28 21:48:36 -0500

    fix projectile weight not being applied properly

--- Assets/Scripts/Projectiles/SpearData.cs
+++ Assets/Scripts/Projectiles/SpearData.cs
@@ -13,11 +13,11 @@ namespace Projectiles
 
         public static SpearData Default => new SpearData
         {
-            speed = 15f,
-            lifetime = 15f,
+            speed = 20f,
+            lifetime = 60f,
             damage = 50f,
             weight = 1.2f,
-            drag = 0f,
+            drag = 0.02f,
             playerDamageMultiplier = 2.1f, // set very high for testing, todo revert to 0.7
             lingerDuration = 20f
         };
