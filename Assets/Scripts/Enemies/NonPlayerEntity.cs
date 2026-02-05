4e0dc0d5f2ae7ef58dbd895a28c9fc1fdc5d5b54
Chris <76500048+Eluminent@users.noreply.github.com>
2026-02-04 20:31:01 -0500

    added attack, attacking data values and movement into DefaultMove

--- Assets/Scripts/Enemies/NonPlayerEntity.cs
+++ Assets/Scripts/Enemies/NonPlayerEntity.cs
@@ -14,23 +14,51 @@ namespace Enemies
         public string entityName = "Enemy";
         public float currHealth = 100f;
         public float maxHealth = 100f;
+        private float moveSpeed = 1f;
+
+        [Header("Attacking")]
+        public float attackDamage = 10f;
+        public float attackRange = 2f;
+        public float attackCooldown = 1f;
+        protected float attackTimer;
 
         [Header("MovementAI")]
-        public bool testRandomMove = true; // TODO remove later
-        private float randomMoveTimer = 0f;
+        public bool useRandomMove = false; // TODO remove later
+        private float randomMoveTimer;
         private Vector2 randomMoveInterval = new Vector2(3f, 5f);
-        private float moveSpeed = 1f;
         private Vector3 moveVelocity = Vector3.zero;
+
+        protected CharacterController controller;
+
+
+        protected virtual void Awake()
+        {
+            controller = GetComponent<CharacterController>();
+        }
 
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
-
+            
         }
 
         // Update is called once per frame
         [Server]
         void Update()
+        {
+            attackTimer -= Time.deltaTime;
+
+            DefaultMove();
+            
+        }
+
+        [Server]
+        public virtual bool CanAttack()
+        {
+            return attackTimer <= 0f;
+        }
+
+        protected void DefaultMove()
         {
             randomMoveTimer -= Time.deltaTime;
             if (testRandomMove && randomMoveTimer <= 0f)
@@ -42,8 +70,27 @@ namespace Enemies
                 ).normalized * moveSpeed;
                 randomMoveTimer = Random.Range(randomMoveInterval.x, randomMoveInterval.y);
             }
-            transform.position += moveVelocity * Time.deltaTime;
+            // transform.position += moveVelocity * Time.deltaTime; 
+            controller.Move(moveVelocity * Time.deltaTime);
         }
+
+
+        [Server]
+        public virtual void Attack(GameObject target)
+        {
+            if (!CanAttack() || target == null) return;
+
+            float dist = Vector3.Distance(transform.position, target.transform.position);
+            if (dist > attackRange) return;
+
+            var damageable = target.GetComponent<IDamageable>();
+            if (damageable != null)
+            {
+                damageable.ApplyDamage(attackDamage);
+                attackTimer = attackCooldown;
+            }
+        }
+
 
         [Server]
         public void ApplyDamage(float damage)
